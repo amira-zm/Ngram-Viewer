95e0fac459408ea71d6d13947956872e01853b57
amira-zm <amiragsm4@icloud.com>
2022-03-01 15:52:08 +0100

    second

--- app.py
+++ app.py
@@ -47,22 +47,31 @@ def index():
 
 @app.route('/favorite')
 def fovorite():
-    trips=[1,2,3,4,5]
+    events= pd.read_csv ('event.csv')
+
+    rec=events.groupby(events.columns.tolist()).size().reset_index(). \
+        rename(columns={0: 'records'})
+    records = rec.head(10)
+    records.to_csv("ev.csv")
 
 
-    return render_template("favorite.html",trips=trips)
+
+
+    return render_template("favorite.html",records=records)
 
 @app.route('/line/<string:form>/<string:corpus>/<string:deb>/<string:end>')
 
 
 def line(form,corpus,deb,end):
     list_date = []
+    poster_path = "https://image.tmdb.org/t/p/original/"
     date = "1800-01-15"
-    desc = "desciiiiiiiiiii"
-    author = "authhhhooor"
-    img = "sharebook.png"
-    genre = "---"
-    imdb = ''
+    desc = ''
+    author = ''
+    img = ''
+    genre = ''
+    d=dict()
+    imdb = []
     ch = form.replace(" ", "") + "-" + corpus + "-" + deb + "-" + end + "-3-caseSensitive.csv"
     fin = open(ch, 'r')
     ngrams = fin.readline().strip().split(',')[1:]
@@ -88,84 +97,11 @@ def line(form,corpus,deb,end):
         if (max_value is None or num > max_value):
             max_value = num
 
-    df = pd.read_csv("C:\\Users\\AMIRA\\Downloads\\final.csv")
+    df = pd.read_csv("final.csv")
+    ind=0
     for i in range(len(df)):
         chaine = df.iloc[i, 1][4:]
-
-        if (form.title() == chaine.title()):
-            print(form.title(), chaine.title())
-            date = df.iloc[i, 2][4:]
-            print(date)
-
-            datem = datetime.datetime.strptime(date, "%Y-%m-%d")
-            try:
-                x = int(values.index(datem.year))
-            except:
-                x=0
-            print(datem.year,'++++++++++',deb,'+++++++++++',end)
-            if(int(datem.year) <=int(end) and int(datem.year)>=int(deb) ):
-                list_date.append(x)
-                print(list_date)
-            print("liiiiiiiiiiiiiiiiiiiiiiiiiiiiiiste", list_date)
-            desc = df.iloc[i, 14][4:]
-            img = df.iloc[i, 15][4:]
-            author = df.iloc[i, 9][4:]
-            genre = df.iloc[i, 11][4:]
-            imdb = df.iloc[i, 16][4:]
-
-    for i in range(len(labels)):
-        labels[i] = (labels[i] * 100) / max_value
-    resultantList = []
-
-    for element in list_date:
-
-        if element not in resultantList :
-
-            resultantList.append(element)
-    print(resultantList)
-
-
-    return render_template('line_chart.html', title='Sharbooks Ngram', max=max_value, labels=values,values=labels,ngrams=ngrams[0],t=resultantList,corpus=corpus,deb=deb,end=end,img=img,desc=desc,author=author,genre=genre,imdb=imdb)
-
-@app.route('/line/<string:form>/<string:corpus>/<string:deb>/<string:end>/<string:id>')
-
-def line1(form,corpus,deb,end,id):
-    list_date = []
-    date = "1800-01-15"
-    desc = "desciiiiiiiiiii"
-    author = "authhhhooor"
-    img = "sharebook.png"
-    genre = "---"
-    imdb = ''
-    ch = form.replace(" ", "") + "-" + corpus + "-" + deb + "-" + end + "-3-caseSensitive.csv"
-    fin = open(ch, 'r')
-    ngrams = fin.readline().strip().split(',')[1:]
-
-    data_vals = [[] for ngram in ngrams]
-    years = []
-    for line in fin:
-        sp = line.strip().split(',')
-        years.append(int(sp[0]))
-        for i, s in enumerate(sp[1:]):
-            data_vals[i].append(float(s) * 100)  # Make percentage
-    fin.close()
-
-    values = []
-    labels = []
-    for i in range(0, len(data_vals[i])):
-        values.append(years[i])
-        labels.append(data_vals[0][i])
-
-    max_value = None
-
-    for num in labels:
-        if (max_value is None or num > max_value):
-            max_value = num
-
-    df = pd.read_csv("C:\\Users\\AMIRA\\Downloads\\final.csv")
-    for i in range(len(df)):
-        chaine = df.iloc[i, 1][4:]
-
+        d1=dict()
         if (form.title() == chaine.title()):
             print(form.title(), chaine.title())
             date = df.iloc[i, 2][4:]
@@ -181,11 +117,18 @@ def line1(form,corpus,deb,end,id):
                 list_date.append(x)
                 print(list_date)
             print("liiiiiiiiiiiiiiiiiiiiiiiiiiiiiiste", list_date)
-            desc = df.iloc[i, 14][4:]
-            img = df.iloc[i, 15][4:]
-            author = df.iloc[i, 9][4:]
-            genre = df.iloc[i, 11][4:]
-            imdb = df.iloc[i, 16][4:]
+            desc=(df.iloc[i, 14][4:])
+            img=(df.iloc[i, 15][4:])
+            author=(df.iloc[i, 9][4:])
+            genre=(df.iloc[i, 11][4:])
+            imdb=(df.iloc[i, 16][4:])
+            d1["img"]=img
+            d1["genre"] = genre
+            d1["author"]=author
+            d1["desc"] = desc
+            d[ind]=d1
+            ind+=1
+
 
     for i in range(len(labels)):
         labels[i] = (labels[i] * 100) / max_value
@@ -197,7 +140,98 @@ def line1(form,corpus,deb,end,id):
             resultantList.append(element)
     print(resultantList)
 
-    return render_template('line_chart1.html', title='Sharbooks Ngram', max=max_value, labels=values,
+    return render_template('line_chart.html', title='Sharbooks Ngram', d=d,max=max_value, labels=values,values=labels,ngrams=ngrams[0],t=resultantList,corpus=corpus,deb=deb,end=end,img=img,desc=desc,author=author,genre=genre,imdb=imdb)
+
+@app.route('/line/<string:form>/<string:corpus>/<string:deb>/<string:end>/<string:id>')
+
+def line1(form,corpus,deb,end,id):
+    list_date = []
+    list_poster=[]
+    list_overview=[]
+    poster_path = "https://image.tmdb.org/t/p/original/"
+    date = "1800-01-15"
+    desc = ''
+    author = ''
+    img = ''
+    genre = ''
+    d = {}
+    imdb = []
+    ch = form.replace(" ", "") + "-" + corpus + "-" + deb + "-" + end + "-3-caseSensitive.csv"
+    fin = open(ch, 'r')
+    ngrams = fin.readline().strip().split(',')[1:]
+
+    data_vals = [[] for ngram in ngrams]
+    years = []
+    for line in fin:
+        sp = line.strip().split(',')
+        years.append(int(sp[0]))
+        for i, s in enumerate(sp[1:]):
+            data_vals[i].append(float(s) * 100)  # Make percentage
+    fin.close()
+
+    values = []
+    labels = []
+    for i in range(0, len(data_vals[i])):
+        values.append(years[i])
+        labels.append(data_vals[0][i])
+
+    max_value = None
+
+    for num in labels:
+        if (max_value is None or num > max_value):
+            max_value = num
+
+    df = pd.read_csv("final.csv")
+    ind = 0
+    for i in range(len(df)):
+        chaine = df.iloc[i, 1][4:]
+        d1 = {}
+        if (form.title() == chaine.title()):
+            print(form.title(), chaine.title())
+            date = df.iloc[i, 2][4:]
+            poster = (df.iloc[i, 17][4:])
+            overview = (df.iloc[i, 18][4:])
+            print(date)
+
+            datem = datetime.datetime.strptime(date, "%Y-%m-%d")
+            try:
+                x = int(values.index(datem.year))
+            except:
+                x = 0
+            print(datem.year, '++++++++++', deb, '+++++++++++', end)
+            if (int(datem.year) <= int(end) and int(datem.year) >= int(deb)):
+                list_date.append(x)
+                list_poster.append(poster)
+                list_overview.append(overview)
+                print(list_date)
+            print("liiiiiiiiiiiiiiiiiiiiiiiiiiiiiiste", list_date)
+            desc = (df.iloc[i, 14][4:])
+            img = (df.iloc[i, 15][4:])
+            author = (df.iloc[i, 9][4:])
+            genre = (df.iloc[i, 11][4:])
+            imdb = (df.iloc[i, 16][4:])
+            d1["img"] = img
+            d1["genre"] = genre
+            d1["author"] = author
+            d1["desc"] = desc
+            d[ind] = d1
+            ind += 1
+
+    for i in range(len(labels)):
+        labels[i] = (labels[i] * 100) / max_value
+    resultantList = []
+    dict= {}
+
+    for i in range(len(list_date)):
+        dicti = {}
+        if list_date[i] not in resultantList:
+            resultantList.append(list_date[i])
+            dicti["poster"]=list_poster[i]
+            dicti["overview"] =list_overview[i]
+            dict[list_date[i]]=dicti
+    print(resultantList)
+
+    return render_template('line_chart1.html', dict=dict,title='Sharbooks Ngram',d=d, max=max_value, labels=values,
                            values=labels,ngrams=ngrams[0],x=id,t=resultantList,corpus=corpus,deb=deb,end=end,desc=desc,img=img,author=author,genre=genre,imdb=imdb)
 
 
